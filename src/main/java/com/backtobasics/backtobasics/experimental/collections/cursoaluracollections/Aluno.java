18cc342a9ae9c3ddccd8c8178a2a45ebca5de11f
Rafael <rafapgbr@gmail.com>
2022-08-08 21:06:11 -0300

    feat: inserção da classe "Aluno".

---
+++ src/main/java/com/backtobasics/backtobasics/experimental/collections/cursoaluracollections/Aluno.java
@@ -0,0 +1,12 @@
+package com.backtobasics.backtobasics.experimental.collections.cursoaluracollections;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Aluno {
+
+    private String nome;
+    private int numeroMatricula;
+}
