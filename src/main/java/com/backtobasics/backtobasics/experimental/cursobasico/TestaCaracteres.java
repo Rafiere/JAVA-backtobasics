3cc3f5a7223dbe94af6a75be726754df53890d5d
Rafael <rafapgbr@gmail.com>
2022-08-08 21:07:42 -0300

    feat: inserção da classe "TestaCaracteres".

---
+++ src/main/java/com/backtobasics/backtobasics/experimental/cursobasico/TestaCaracteres.java
@@ -0,0 +1,11 @@
+package com.backtobasics.backtobasics.experimental.cursobasico;
+
+public class TestaCaracteres {
+
+    public static void main(String[] args) {
+
+        char letra = 'A';
+
+        System.out.println(letra);
+    }
+}
