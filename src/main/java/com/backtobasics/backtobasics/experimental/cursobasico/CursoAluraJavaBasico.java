49fab24fcbca9845727d84e2834d2828ef4c814a
Rafael <rafapgbr@gmail.com>
2022-08-08 21:08:15 -0300

    feat: inserção da classe "CursoAluraJavaBasico".

---
+++ src/main/java/com/backtobasics/backtobasics/experimental/cursobasico/CursoAluraJavaBasico.java
@@ -0,0 +1,17 @@
+package com.backtobasics.backtobasics.experimental.cursobasico;
+
+public class CursoAluraJavaBasico {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá, mundo!");
+
+        int idade = 100;
+
+        System.out.println(idade);
+
+        idade = 100 + 100;
+
+        System.out.println(idade);
+    }
+}
