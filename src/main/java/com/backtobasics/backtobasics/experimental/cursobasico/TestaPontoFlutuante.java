d853fab140272e13f4119edc6e3045b97c4bf7cb
Rafael <rafapgbr@gmail.com>
2022-08-08 21:07:36 -0300

    feat: inserção da classe "TestaPontoFlutuante".

---
+++ src/main/java/com/backtobasics/backtobasics/experimental/cursobasico/TestaPontoFlutuante.java
@@ -0,0 +1,12 @@
+package com.backtobasics.backtobasics.experimental.cursobasico;
+
+public class TestaPontoFlutuante {
+
+    public static void main(String[] args) {
+
+        double numeroTeste = 37.5;
+
+        System.out.println(numeroTeste);
+
+    }
+}
